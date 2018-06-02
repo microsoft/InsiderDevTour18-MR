1ffd82c63c0497a4f4cf744b4ce77626d57e58e9
Jaime Rodriguez <jaimer@live.com>
2018-06-02 11:01:29 -0700

    made cursor larger for demo purposes

---
+++ Assets/Scripts/SpeechInputFeedback.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine; 
+
+
+[RequireComponent(typeof(AudioSource))]
+public class SpeechInputFeedback : MonoBehaviour {
+
+    private AudioSource audio;
+
+    public AudioClip positiveFeedbackClip;
+    public AudioClip negativeFeedbackClip; 
+	// Use this for initialization
+	void Start () {
+        audio = GetComponent<AudioSource>(); 
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Play ( bool isPositive = true )
+    {
+        if (audio != null)
+        {
+            if (isPositive && positiveFeedbackClip != null )                
+                audio.PlayOneShot(positiveFeedbackClip, 1f);
+            else if  ( negativeFeedbackClip != null )
+                audio.PlayOneShot(negativeFeedbackClip, 1f); 
+        }
+            
+    }
+
+    public void Stop ()
+    {
+        if (audio != null)
+            audio.Stop(); 
+    }
+
+    public void Pause ()
+    {
+        if (audio != null )
+        {
+            audio.Pause(); 
+        }
+    }
+
+
+}
